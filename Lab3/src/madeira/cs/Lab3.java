6b6573019661b800cbd6f4f3fd79611299723b40
Eliza Reed <ereed@madeira.org>
2020-09-17 10:44:38 -0400

    lab 3

--- Lab3/src/madeira/cs/Lab3.java
+++ Lab3/src/madeira/cs/Lab3.java
@@ -23,20 +23,22 @@ public class Lab3 {
         // Part I code below
 System.out.println ("Please enter your age in years.");
 
+// Creating a scanner for the user to input their age and declaring the input as an integer
 Scanner ageScanner = new Scanner(System.in);
 int age = ageScanner.nextInt();
 
+// Checking if the age is over 16 to see if they can drive with a permit, and over 18 to see if they can get a license.
 if (age < 18) {
     if (age < 16)
         System.out.println ("You're not old enough to drive legally in the US.");
     else
         System.out.println ("In most US states, you can drive legally with a learner's permit or junior license.");
-
 }
 else {
    System.out.println ("You're old enough to drive legally in the US if you have a license.");
 }
 
+// Checking if their age is over 18 to see if they can get a tattoo.
 if (age >= 18) {
     System.out.println ("You're old enough to get a tattoo, for better or worse.");
 }
@@ -44,18 +46,23 @@ else {
     System.out.println ("You're not old enough to get a tattoo, but that's probably for the best.");
 }
 
+
+// Checking if their age is over 37 to see if they can run for president.
 if (age >= 37) {
     System.out.println ("You're old enough to run for president of the US. (Start planning your campaign now!)");
 }
 else {
     System.out.println ("In case you were thinking about it, you're not old enough to run for president of the US.");
 }
+
         // Part II code below
+// A scanner for the user to enter their birth month.
 System.out.println ("Please enter your birth month.");
 Scanner monthScanner = new Scanner (System.in);
 String month = monthScanner.nextLine();
 
-
+// And here we have a group of if/else if/else statements based on what their birth month is.
+// For each month, there is a statement with its season, number of days, and possible astrological signs.
 if (month.equals("January")) {
     System.out.println("January is in the winter, and it has 31 days.");
     System.out.println("You're either a Capricorn or an Aquarius.");
@@ -104,17 +111,95 @@ else if (month.equals ("December")) {
     System.out.println ("December is in the winter, and it has 31 days.");
     System.out.println ("You're either a Sagittarius or a Capricorn.");
 }
+
+// Else statement just in case the user enters input that isn't a month.
 else {
   System.out.println ("Sorry, I didn't recognize that as a month.");
 }
 
         // Part III code below
-System.out.println ("Please enter your age in years.");
-Scanner ageScanner2 = new Scanner (System.in);
-int age2 = ageScanner2.nextInt();
 
-int days = (age2 * 365);
 
-System.out.println ("You have been alive for approximately " +days +" days.");
+// Multiplying the value of their age in years by 365 to get the number of days for the years leading up to this one.
+int days1 = (age * 365);
+
+System.out.println ("Please enter the day of the month you were born.");
+// Scanner for the user to enter their birth day.
+Scanner dayScanner = new Scanner (System.in);
+// Declaring the input as an integer.
+int birthday = dayScanner.nextInt();
+
+/*
+This series of if/else if/else  statements specifies what to print for each month. For dates that have passed,
+it takes the amount of days that have passed in the years leading up to this one (days1) and adds it to the number of
+days that have passed since their most recent birthday. This number of days is created by taking the number of days that
+have already passed this year and subtracting the number of days that passed before their birthday this year.
+ */
+// Please keep in mind that this code was written on September 16, 2020, and will be slightly out of date.
+if (month.equals("January") && birthday <= 31) {
+    System.out.println ("You have been alive for about "+(days1 + 260 - birthday) +" days.");
+}
+else if (month.equals("February") && birthday <= 29) {
+    System.out.println ("You have been alive for about " +(days1 + 260 - birthday - 31) +" days.");
+}
+else if (month.equals ("March") && birthday <= 31) {
+    System.out.println ("You have been alive for about " +(days1 + 260 - birthday - 60) +" days.");
+}
+else if (month.equals ("April") && birthday <= 30) {
+    System.out.println ("You have been alive for about " +(days1 + 260 - birthday - 91) +" days");
+}
+else if (month.equals ("May") && birthday <= 31) {
+    System.out.println ("You have been alive for about " +(days1 + 260 - birthday - 121) +" days.");
+}
+else if (month.equals ("June") && birthday <= 30) {
+    System.out.println ("You have been alive for about " +(days1 + 260 - birthday - 152) +" days.");
+}
+else if (month.equals ("July") && birthday <= 31) {
+    System.out.println ("You have been alive for about " +(days1 + 260 - birthday - 182) +" days.");
+}
+else if (month.equals ("August") && birthday <= 31) {
+    System.out.println ("You have been alive for about " +(days1 + 260 - birthday - 213) +" days.");
+}
+/*
+September is weird because we're in it right now, so it transitions in the middle of the month from dates that have
+already happened to dates that have not. In the beginning of the month, we use the same formula as before. For
+today's date, we simply add days1 to the amount of days that have already passed in 2020. For days after today in
+September, we add days1 to 365 (a whole year) and then subtract birthday - 16 to subtract the number of days that will
+pass before their birthday.
+ */
+else if (month.equals ("September") && birthday <= 30) {
+    if (birthday < 16) {
+        System.out.println ("You have been alive for about " +(days1 + 365 - birthday - 244) +" days.");
+    }
+    else if (birthday == 16) {
+        System.out.println ("Happy birthday! You have been alive for about " +(days1 + 260) +" days.");
+    }
+    else {
+        System.out.println ("You have been alive for about " +(days1 + 365 + (birthday - 16)) +" days.");
+    }
+/*
+In October, we begin with dates that have not happened yet, and we have to change our formula to find the amount of
+days the user has been alive. We have to take days1 and add it to the number of days that have passed since their most
+recent birthday. (We can get that number of days by adding the number of days that has passed in 2020 to the number
+of days between their last birthday and the beginning of 2020.)
+ */
+}
+else if (month.equals ("October") && birthday <= 31) {
+    System.out.println ("You have been alive for about " +(days1 + 260 + (92 - birthday)) +" days.");
+}
+else if (month.equals ("November") && birthday <= 30) {
+    System.out.println ("You have been alive for about " +(days1 + 260 + (61 - birthday)) +" days.");
+}
+else if (month.equals ("December") && birthday <= 31) {
+    System.out.println ("You have been alive for about " +(days1 + 260 + (31 - birthday)) +" days.");
+}
+// Else statement just in case the user enters input that isn't a month, or a birthday that doesn't exist.
+else {
+    System.out.println ("Sorry, it looks like you entered invalid input.");
+}
+
+
+
+
     }
 }
